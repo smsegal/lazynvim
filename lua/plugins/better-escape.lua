6bff584a25693c1c959a2fe5b10752b1c4b113f2
Shane Segal <shane@smsegal.ca>
2023-01-25 09:13:12 -0800

    initial plugins added on top (better-escape, etc)

---
+++ lua/plugins/better-escape.lua
@@ -0,0 +1,4 @@
+return {
+  "max397574/better-escape.nvim",
+  opts = { mapping = { "fd" }, clear_empty_lines = true },
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,43 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>ff", "<cmd>Telescope file_browser<cr>", "browse files" },
+  },
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+      file_browser = {
+        theme = "ivy",
+        hijack_netrw = true,
+      },
+    },
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = require("telescope.actions").move_selection_next,
+          ["<C-k>"] = require("telescope.actions").move_selection_previous,
+        },
+      },
+    },
+  },
+  dependencies = {
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+      config = function()
+        require("telescope").load_extension("file_browser")
+      end,
+    },
+  },
+}
