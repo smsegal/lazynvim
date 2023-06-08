0e0ff71ab73c907143eb5ca843d55a07a20781d3
Shane Segal <shane.segal@perceive.io>
2023-06-08 09:49:59 -0700

    fix: add nix commentstring

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,3 +10,20 @@
 --     vim.b.autoformat = false
 --   end,
 -- })
+
+local function is_filetype(filetype)
+  return vim.bo.filetype == filetype
+end
+
+local function set_commentstring()
+  if is_filetype("nix") then
+    vim.bo.commentstring = "# %s"
+  end
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*" },
+  callback = function()
+    set_commentstring()
+  end,
+})
